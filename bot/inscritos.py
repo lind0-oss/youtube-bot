d2b7a5b223e2de8893e03605d0b901670ee0220d
antonio <79468599+lind0-oss@users.noreply.github.com>
2021-06-20 11:20:38 -0300

    pequenas mudanças

---
+++ bot/inscritos.py
@@ -0,0 +1,7 @@
+import pyautogui
+from time import sleep
+from random import choice, shuffle
+import sys
+import os
+import webbrowser
+
---
+++ bot/viewers.py
@@ -0,0 +1,6 @@
+import pyautogui
+from time import sleep
+from random import choice, shuffle
+import sys
+import os
+import webbrowser
