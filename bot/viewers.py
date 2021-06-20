c31860cddd53b47e1b86ccdfa0a14355172671db
antonio <79468599+lind0-oss@users.noreply.github.com>
2021-06-20 13:26:37 -0300

    mudando algumas coisas

--- bot/viewers.py
+++ bot/viewers.py
@@ -6,13 +6,13 @@ import sys
 import webbrowser
 
 linkOne = input("Primeiro link\n >  ")
-linkTwp = input("Segundo link\n >  ")
+#linkTwp = input("Segundo link\n >  ")
 
 nviews = int(input("Quantas views deseja?\n >  "))
 
 tamanho_do_video = int(input("Tamanho do vídeo\n >  "))
 
-tamanho_do_video2 = int(input("Tamanho do vídeo\n >  "))
+#tamanho_do_video2 = int(input("Tamanho do vídeo\n >  "))
 
 # tempo de espera personalizado
 tempodeespera = int(input("quantos segundos para inicializar\n >  "))
@@ -20,15 +20,16 @@ tempodeespera = int(input("quantos segundos para inicializar\n >  "))
 #vou testar se funciona
 sleep(tempodeespera)
 
-def verificador_de_link(link):
+bot(linkOne)
+
+def bot(link):
     verificador = 'https://'
-    zero = 0
     if verificador not in link:
         print("Isto não é um link =(")
         sleep(5)
         sys.exit()
     else:
-        while zero <= nviews:
+        while 0 < nviews:
             pyautogui.sleep(1)
             webbrowser.open_new_tab(linkOne)
             pyautogui.sleep(1)
@@ -36,12 +37,13 @@ def verificador_de_link(link):
             pyautogui.sleep(tamanho_do_video)
             pyautogui.sleep(1)
             pyautogui.hotkeys("ctrl", "w")
-            webbrowser.open_new_tab(linkTwp)
-            pyautogui.sleep(1)
-            pyautogui.press("space")
-            pyautogui.sleep(tamanho_do_video2)
-            pyautogui.sleep(1)
-            pyautogui.hotkeys("ctrl", "w")
+            pyautogui.sleep(10)
+            #webbrowser.open_new_tab(linkTwp)
+            #pyautogui.sleep(1)
+            #pyautogui.press("space")
+            #pyautogui.sleep(tamanho_do_video2)
+            #pyautogui.sleep(1)
+            #pyautogui.hotkeys("ctrl", "w")
 print("Fim do script!")
 sleep(5)
 sys.exit()
