c952e281ad1192c57be1568962b426382283a74d
antonio <79468599+lind0-oss@users.noreply.github.com>
2021-06-20 12:51:40 -0300

    nada

--- bot/viewers.py
+++ bot/viewers.py
@@ -4,3 +4,16 @@ from random import choice, shuffle
 import sys
 import os
 import webbrowser
+
+# tempo de espera personalizado
+tempodeespera = int(input("quantos segundos para inicializar\n >  "))
+
+#vou testar se funciona
+sleep(tempodeespera)
+
+link1 = 'https://youtu.be/445WBjoK2O8'
+link2 = 'https://youtu.be/tW9sQHTqsbc'
+
+webbrowser.open_new_tab(link1)
+pyautogui.press('space')
+webbrowser.open_new_tab(link2)
