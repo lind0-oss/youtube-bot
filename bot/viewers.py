3aef898259e101ec3224bfae1754a4c2d60510b4
Los-had <githubtestefinal@gmail.com>
2021-06-23 16:14:47 -0300

    nada

--- bot/viewers.py
+++ bot/viewers.py
@@ -23,8 +23,8 @@ def bot(link):
         print("Isto não é um link =(")
         print('Porém será pesquisado =)')
         #nr = int(input("Quantos resultados deseja?\n >  "))
-        webbrowser.open_new_tab(link)
-        for resultado in search('"{link}" google', stop=200):
+        #webbrowser.open_new_tab(link)
+        for resultado in search('"{link}" google', stop=150):
             print(f'Resultado: {resultado}')
         sleep(5)
         dsd = input("Deseja sair? (y/n)\n >  ")
